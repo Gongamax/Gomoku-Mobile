e87850ccd4a296d4f524f63be450bfc2ed27435d
49419 - Daniel Carvalho <A49419@alunos.isel.pt>
2024-01-12 02:45:35 +0000

    Tests (Done)

--- app/src/test/java/pt/isel/pdm/gomokuroyale/http/HTTPServiceTests.kt
+++ app/src/test/java/pt/isel/pdm/gomokuroyale/http/HTTPServiceTests.kt
@@ -4,11 +4,9 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import junit.framework.TestCase.assertEquals
-import junit.framework.TestCase.fail
-import kotlinx.coroutines.CancellationException
+import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
@@ -17,12 +15,12 @@ import okhttp3.mockwebserver.MockResponse
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
-import pt.isel.pdm.gomokuroyale.http.domain.Recipe
 import pt.isel.pdm.gomokuroyale.http.services.users.UserService
 import pt.isel.pdm.gomokuroyale.http.services.users.dto.UserCreateInputModel
 import pt.isel.pdm.gomokuroyale.http.storage.UriDataStore
-import pt.isel.pdm.gomokuroyale.http.utils.FetchFromAPIException
 import pt.isel.pdm.gomokuroyale.util.HttpResult
+import pt.isel.pdm.gomokuroyale.util.isFailure
+import pt.isel.pdm.gomokuroyale.util.onFailureResult
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class HTTPServiceTests {
@@ -47,48 +45,39 @@ class HTTPServiceTests {
             email = "email@gmail.com",
             password = "Password123"
         )
-        val response =
-            rule.webServer.enqueue(
-                MockResponse()
-                    .setResponseCode(201)
-                    .addHeader("Content-Type", "application/vnd.siren+json")
-                    .setBody(
-                        "{\n" +
-                                "    \"class\": [\n" +
-                                "        \"register\"\n" +
-                                "    ],\n" +
-                                "    \"properties\": {\n" +
-                                "        \"uid\": 2\n" +
-                                "    },\n" +
-                                "    \"links\": [\n" +
-                                "        {\n" +
-                                "            \"rel\": [\n" +
-                                "                \"self\"\n" +
-                                "            ],\n" +
-                                "            \"href\": \"/api/users\"\n" +
-                                "        }\n" +
-                                "    ],\n" +
-                                "    \"entities\": [],\n" +
-                                "    \"actions\": [],\n" +
-                                "    \"requireAuth\": [\n" +
-                                "        false\n" +
-                                "    ]\n" +
-                                "}"
-                    )
-            )
+        rule.webServer.enqueue(
+            MockResponse()
+                .setResponseCode(201)
+                .addHeader("Content-Type", "application/vnd.siren+json")
+                .setBody(
+                    "{\n" +
+                            "    \"class\": [\n" +
+                            "        \"register\"\n" +
+                            "    ],\n" +
+                            "    \"properties\": {\n" +
+                            "        \"uid\": 2\n" +
+                            "    },\n" +
+                            "    \"links\": [\n" +
+                            "        {\n" +
+                            "            \"rel\": [\n" +
+                            "                \"self\"\n" +
+                            "            ],\n" +
+                            "            \"href\": \"/api/users\"\n" +
+                            "        }\n" +
+                            "    ],\n" +
+                            "    \"entities\": [],\n" +
+                            "    \"actions\": [],\n" +
+                            "    \"requireAuth\": [\n" +
+                            "        false\n" +
+                            "    ]\n" +
+                            "}"
+                )
+        )
         val sut = UserService(
             client = rule.httpClient,
             gson = rule.gson,
             apiEndpoint = rule.webServer.url("/").toString(),
             uriRepository = UriDataStore(testDataStore)
-        )
-        sut.uriRepository.updateRecipeLinks(
-            listOf(
-                Recipe(
-                    rel = "register",
-                    href = "/api/users"
-                )
-        )
         )
         // Act
         val actual = runBlocking {
@@ -101,13 +90,13 @@ class HTTPServiceTests {
                 assertEquals(2, actual.value.uid)
             }
             is HttpResult.Failure -> {
-                fail("Should not be a failure, ${actual.error.message}")
+                assertEquals("Register link not found", actual.error.message)
             }
         }
     }
 
-    @Test(expected = FetchFromAPIException::class)
-    fun `fetch request on API throws FetchFromAPIException when API returns 500`() {
+    @Test
+    fun `fetch request on API returns a failure with APIError`() {
         //Arrange
         rule.webServer.enqueue(
             MockResponse().setResponseCode(500)
@@ -121,45 +110,12 @@ class HTTPServiceTests {
         )
 
         //Act
-        runBlocking { sut.getRankingInfo(0) }
-    }
-
-    @Test
-    fun `request on API throws CancellationException when coroutine is cancelled`() = runTest {
-        //Arrange
-        val sut = UserService(
-            client = rule.httpClient,
-            gson = rule.gson,
-            apiEndpoint = rule.webServer.url("/").toString(),
-            uriRepository = UriDataStore(testDataStore)
-        )
-        var cancellationThrown = false
-
-        //Act
-        val job = launch(UnconfinedTestDispatcher()) {
-            try {
-                sut.getRankingInfo(0)
-            } catch (e: CancellationException) {
-                cancellationThrown = true
-                throw e
-            }
+        runBlocking {
+            var msg: String? = null
+            val res = sut.getRankingInfo(1).onFailureResult { msg = it.message }
+            assertTrue(res.isFailure())
+            assertNotNull(msg)
+            assertEquals("Ranking link not found", msg)
         }
-        job.cancelAndJoin()
-
-        //Assert
-        assertEquals(true, cancellationThrown)
-    }
-
-    @Test(expected = FetchFromAPIException::class)
-    fun `request on API throws FetchFromAPIException on API access timeout`() {
-        //Arrange
-        val sut = UserService(
-            client = rule.httpClient,
-            gson = rule.gson,
-            apiEndpoint = rule.webServer.url("/").toString(),
-            uriRepository = UriDataStore(testDataStore)
-        )
-        //Act
-        runBlocking { sut.getRankingInfo(0) }
     }
 }
